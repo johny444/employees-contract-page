8330ca644ea8fa744b95ae61a9583579099bcc1e
johny444 <johnyjohnny4@gmail.com>
2025-01-08 09:15:50 +0700

    first

--- src/components/contract/index.vue
+++ src/components/contract/index.vue
@@ -8,7 +8,7 @@
       </v-row>
       <div class="action">
         <v-row>
-          <h3 class="label px-4">FROM</h3>
+          <h3 class="label px-5">FROM</h3>
           <v-col cols="2">
             <v-text-field
               density="compact"
@@ -29,69 +29,72 @@
           </v-col>
         </v-row>
       </div>
-      <div class="mt-5">
-        <v-table>
-          <thead>
-            <tr>
-              <th>{{ $t("rowNum") }}</th>
-              <th>{{ $t("subject") }}</th>
-              <th>{{ $t("createDate") }}</th>
-              <th>{{ $t("class") }}</th>
-              <th>{{ $t("term") }}</th>
-              <th>ACTION</th>
-            </tr>
-          </thead>
-          <tbody>
-            <template v-if="tableData.length > 0">
-              <tr v-for="(item, i) in tableData" :key="item.name">
-                <td width="5%" align="center">{{ i + 1 }}</td>
-                <td width="10%">{{ item.classExamid.subjectExam }}</td>
-                <td width="20%">{{ item.time }}</td>
-                <td width="10%">{{ item.classExamid.classExam }}</td>
-                <td width="10%">
-                  {{ item.term === "mid" ? $t("midterm") : $t("finalterm") }}
-                </td>
-                <td width="12%">
-                  <v-row justify="center" style="margin: 1rem">
-                    <!-- <NuxtLink :to="`/quiz/${item.id}`"> -->
-                    <v-btn
-                      variant="text"
-                      density="compact"
-                      icon="fa-solid fa-plus"
-                      size="medium"
-                      @click="startExam(item.id)"
-                    >
-                      <v-icon
-                        style="color: #046b5a"
-                        icon="fa-solid fa-file-circle-question"
-                      />
-                      <v-tooltip activator="parent" location="start">{{
-                        $t("startExam")
-                      }}</v-tooltip>
-                    </v-btn>
-                    <!-- </NuxtLink> -->
-                  </v-row>
-                </td>
-              </tr>
-            </template>
-            <template v-else>
-              <td :colspan="9">
-                <div style="margin-top: 1rem; text-align: center">
-                  <NoData></NoData>
-                </div>
-              </td>
-            </template>
-          </tbody>
-          <template #bottom v-if="tableData.length > 0">
-            <table-pagination
-              :itemList="store.examList.length"
-              :perPage="perPage"
-              @valueChange="onPageChange"
-            ></table-pagination>
-          </template>
-        </v-table>
-      </div>
     </div>
+    <v-container max-width="90%">
+      <v-table>
+        <thead>
+          <tr>
+            <th>{{ $t("rowNum") }}</th>
+            <th>{{ $t("NAME") }}</th>
+            <th>{{ $t("CONTRACT NO") }}</th>
+            <th>{{ $t("DURATION") }}</th>
+            <th>{{ $t("STARTDATE") }}</th>
+            <th>{{ $t("ENDDATE") }}</th>
+            <th>{{ $t("POSITION") }}</th>
+            <th>{{ $t("DEPARTMENT") }}</th>
+            <th>ACTION</th>
+          </tr>
+        </thead>
+        <tbody>
+          <template v-if="tableData.length > 0">
+            <tr v-for="(item, i) in tableData" :key="item.name">
+              <td width="5%" align="center">{{ i + 1 }}</td>
+              <td width="10%">{{ item.classExamid.subjectExam }}</td>
+              <td width="20%">{{ item.time }}</td>
+              <td width="10%">{{ item.classExamid.classExam }}</td>
+              <td width="10%">
+                {{ item.term === "mid" ? $t("midterm") : $t("finalterm") }}
+              </td>
+              <td width="12%">
+                <v-row justify="center" style="margin: 1rem">
+                  <!-- <NuxtLink :to="`/quiz/${item.id}`"> -->
+                  <v-btn
+                    variant="text"
+                    density="compact"
+                    icon="fa-solid fa-plus"
+                    size="medium"
+                    @click="startExam(item.id)"
+                  >
+                    <v-icon
+                      style="color: #046b5a"
+                      icon="fa-solid fa-file-circle-question"
+                    />
+                    <v-tooltip activator="parent" location="start">{{
+                      $t("startExam")
+                    }}</v-tooltip>
+                  </v-btn>
+                  <!-- </NuxtLink> -->
+                </v-row>
+              </td>
+            </tr>
+          </template>
+          <template v-else>
+            <td :colspan="9">
+              <div style="margin-top: 1rem; text-align: center">
+                <NoData></NoData>
+              </div>
+            </td>
+          </template>
+        </tbody>
+        <template #bottom v-if="tableData.length > 0">
+          <table-pagination
+            :itemList="store.examList.length"
+            :perPage="perPage"
+            @valueChange="onPageChange"
+          ></table-pagination>
+        </template>
+      </v-table>
+    </v-container>
   </div>
 </template>
 
@@ -114,9 +117,7 @@ export default {
   /* border: 0.1rem solid;
   border-color: red; */
 }
-.action {
-  min-height: 100vh;
-}
+
 .label {
   margin: 1rem 0 1rem 0;
 }
